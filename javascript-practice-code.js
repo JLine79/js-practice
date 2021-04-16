614a951686d80f9c02b8b55c3210f1826b342f87
Jennifer Line <51475586+JLine79@users.noreply.github.com>
2021-04-16 14:39:52 +0100

    Up to question 21

--- javascript-practice-code.js
+++ javascript-practice-code.js
@@ -349,4 +349,9 @@ function checkPolarity () {
   console.log(checkNumbers);
 }
 
+//21. Write a JavaScript program to create a new string adding "Py" in front of a given string. 
+//If the given string begins with "Py" then return the original string. 
+
+
+
 
